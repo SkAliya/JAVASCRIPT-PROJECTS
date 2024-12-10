6392c8938c6aa6b7d45289792e62d950cfa90a5f
Shaik Aliya <skaliya205@gmail.com>
2024-12-10 21:44:05 +0530

    :tada:Project-2

--- Project-2/script.js
+++ Project-2/script.js
@@ -3,23 +3,38 @@ const total = document.querySelector(".container__total");
 const btns = document.querySelector(".container__grid");
 
 inp.focus();
-let num = 0;
+let num = "";
+
+// TAKING INPUT VALUE
 function doMath(e) {
   if (e.target.dataset.type === "number") {
-    inp.value += e.target.dataset.value;
-    num += +e.target.dataset.value;
-    // total.textContent = eval(inp.value);
-    // total.textContent = ;
-    // num += +e.target.dataset.value;
+    //DISPLAYING SUM OF INPUT WHEN SELECT =
+    if (num.endsWith("=")) {
+      inp.value = "";
+      total.textContent = "";
+      num = "";
+    }
+    inp.value = "";
+    //DISPLAYING NEW DIGIT IN INPUTBAR
+    inp.value = e.target.dataset.value;
+    num += inp.value;
+    inp.value = num.replace("*", "x");
+    //CHANGE THE FONT-SIZE WHEN INP LENGTH IS HIGHER
+    if (inp.value.length > 10) {
+      inp.classList.add("extraInput");
+    }
+
+    inp.focus();
   }
+  //REDIRECT TO CALC FUN WHEN ANY SYMBOL SELECTS
   if (e.target.dataset.type === "symbol") {
+    console.log("entered");
     calc(e.target);
   }
 }
 
+//CHECKS WHAT BTN SELECTS ND CALL A/C FUNC
 function calc(target) {
-  console.log(target.dataset.value);
-
   switch (target.dataset.value) {
     case "ac":
       clear(target);
@@ -28,19 +43,19 @@ function calc(target) {
       del(target);
       break;
     case "%":
-      mod(target);
+      outPut(target);
       break;
-    case "∕":
-      divide(target);
+    case "/":
+      outPut(target);
       break;
     case "x":
       times(target);
       break;
     case "-":
-      sub(target);
+      outPut(target);
       break;
     case "+":
-      add(target);
+      outPut(target);
       break;
     case "=":
       sum(target);
@@ -51,24 +66,61 @@ function calc(target) {
   }
 }
 
-btns.addEventListener("click", doMath);
-
-function add(target) {
-  inp.value += target.dataset.value;
-  inp.focus();
-  // console.log(num);
+//REPLACING SYMBOL IF MULTIPLE TYMS SAME SYMBOL SELECTED
+function replaceExtra(target) {
+  let regex = /[0-9]/g;
+  let result = regex.test(num[num.length - 1])
+    ? (num += target.dataset.value)
+    : num.slice(0, -1) + num.at(-1).replace(num.at(-1), target.dataset.value);
+  num = result;
+  console.log(num);
 }
 
-function sub(target) {
-  console.log(target.dataset.value, inp.value);
-  inp.value += target.dataset.value;
+//DISPLAYING OUTPUT IN INPUTBAR & OUTPUT TAB
+function outPut(target) {
+  num = String(num);
   inp.focus();
-  // console.log(num);
+  replaceExtra(target);
+  total.textContent = eval(num.slice(0, -1)) + num.at(-1);
+  inp.value = total.textContent;
+  num = total.textContent;
 }
 
+//DOING MULTIPLICATION & REPLACING X -> *
 function times(target) {
-  console.log(target.dataset.value, inp.value);
-  inp.value += target.dataset.value;
+  num = String(num);
   inp.focus();
-  // console.log(num);
+  replaceExtra(target);
+  total.textContent = eval(num.slice(0, -1)) + num.slice(-1);
+  inp.value = total.textContent;
+  num = total.textContent;
+  num = num.replace("x", "*");
 }
+
+//DELETING INPUT SINGLE SINGLE
+function del(target) {
+  num = num.slice(0, num.length - 1);
+  inp.value = num.replace("*", "x");
+  total.textContent = num.replace("*", "x");
+  inp.focus();
+}
+
+//CLEARING ALL INPUT & OUTPUT
+function clear(target) {
+  inp.value = "";
+  inp.focus();
+  num = "";
+  total.textContent = 0;
+}
+
+//SHOWING FINAL EQUAL VALUE
+function sum(target) {
+  num = String(num);
+  inp.focus();
+  replaceExtra(target);
+  total.textContent = num;
+  inp.value = eval(num.slice(0, -1));
+}
+
+//HANDLING EVENT
+btns.addEventListener("click", doMath);
--- README.md
+++ README.md
@@ -13,6 +13,21 @@ This repo contains all JavaScript projects🗃️ from Newbie🌱 to Advance �
 
  <br/>
 
- Live Site URL: [Live Demo](https://leafy-empanada-416b13.netlify.app/)
+Live Site URL: [Live Demo](https://leafy-empanada-416b13.netlify.app/)
+
+</div>
+
+<div align="center">
+ <h2 align="center">Project-2</h2>
+
+ <div align="center">
+ 
+ ![solution preview](images/project-2.png)
+ 
+ </div>
+
+ <br/>
+
+Live Site URL: [Live Demo](https://reliable-pie-e5bc31.netlify.app/)
 
 </div>
